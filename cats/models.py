54c54e38244aaf984d3fd1ba8df61e2d4cc3364d
Kirill Efremov <stchristoff82@gmail.com>
2023-04-24 00:27:14 +0300

    Final

--- cats/models.py
+++ cats/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+CHOICES = (
+        ('Gray', 'Серый'),
+        ('Black', 'Чёрный'),
+        ('White', 'Белый'),
+        ('Ginger', 'Рыжий'),
+        ('Mixed', 'Смешанный'),
+    )
+
 
 class Achievement(models.Model):
     name = models.CharField(max_length=64)
@@ -14,11 +22,11 @@ class Owner(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
-    
+
 
 class Cat(models.Model):
     name = models.CharField(max_length=16)
-    color = models.CharField(max_length=16)
+    color = models.CharField(max_length=16, choices=CHOICES)
     birth_year = models.IntegerField()
     owner = models.ForeignKey(
         'Owner',
